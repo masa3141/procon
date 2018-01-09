07a93744796a58b62294736507af345f398a0d4b
mk <masahiro@kazama.tv>
2018-01-09 23:35:15 +0900

    solve ARC

--- Atcoder/AGC/019/A.py
+++ Atcoder/AGC/019/A.py
@@ -3,14 +3,19 @@ import math
 Q, H, S, D = map(int, input().split())
 N = int(input())
 
-data = [Q*4, H*2, S, D/2]
-nums = [0.25, 0.5, 1, 2]
-price = [Q, H, S, D]
-data_index = sorted(range(len(data)), key=lambda k: data[k])
-print(data,data_index)
-ans = 0
-for i in range(4):
-    print(i,data_index[i], N//nums[data_index[i]], N//nums[data_index[i]] * price[data_index[i]])
-    ans += N//nums[data_index[i]] * price[data_index[i]]
-    N -= N//nums[data_index[i]]
-print(N, ans)
+one = min([Q*4, H*2, S])
+two = D
+
+cost1 = N * one
+cost2 = N//2 * two + (N % 2) * one
+#print(one, two, cost1, cost2)
+# data = [Q*4, H*2, S, D/2]
+# nums = [0.25, 0.5, 1, 2]
+# price = [Q, H, S, D]
+# data_index = sorted(range(len(data)), key=lambda k: data[k])
+# #print(data,data_index)
+# ans = 0
+# for i in range(4):
+#     ans += N//nums[data_index[i]] * price[data_index[i]]
+#     N -= N//nums[data_index[i]] * nums[data_index[i]]
+print(min(cost1, cost2))
--- Atcoder/ARC/083/C.py
+++ Atcoder/ARC/083/C.py
@@ -2,18 +2,44 @@
 
 '''
 戦略
-溶ける限界かつ容量限界まで砂糖を加える
-溶ける限界が来たら、少ない方の水を足す
-それぞれの過程の量を覚えておき、最大のときの量を出力する
+すべてのパターンをfor文で回す
 '''
 
 A, B, C, D, E, F = map(int, input().split())
 
 V = []
-max_v = 0
-max_ind = -1
+max_water = 0
+max_sato = 0
+max_ratio = -1
 
-water = 0
+maxA = F//(100*A)
+maxB = F//(100*B)
 
-while True:
-    if
+waters = set()
+
+for i in range(0, maxA+1):
+    for j in range(0, maxB+1):
+        water = 100*A*i + 100*B*j
+        waters.add(water)
+
+maxC = F // C
+maxD = F // D
+satos = set()
+for k in range(0, maxC+1):
+    for l in range(0, maxD+1):
+        sato = C*k + D*l
+        satos.add(sato)
+
+for water in waters:
+    for sato in satos:
+        if water+sato > F or water+sato == 0:
+            continue
+        if sato > water/100 * E:
+            continue
+        #print(water, sato)
+        ratio = sato/(water+sato)*100
+        if ratio > max_ratio:
+            max_water = water+sato
+            max_sato = sato
+            max_ratio = ratio
+print(max_water, max_sato)
